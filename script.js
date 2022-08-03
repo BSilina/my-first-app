69590a11be4ce21bdac910159a95112821dcd0af
BSilina <vasylynabatiuk@gmail.com>
2022-08-03 21:54:27 +0300

    add functions(format sunrise, format sunset, forecast date, format day,  display forecast response,  forecast coordinates,  initMap(autocomplete))

--- script.js
+++ script.js
@@ -31,6 +31,216 @@ document.querySelector(
   "#current-time"
 ).innerHTML = `${day}, ${currentDate} ${month}`;
 
+function formatSunrise(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours.toString().length < 2) {
+    hours = `0${hours}`;
+  }
+
+  let minutes = date.getMinutes();
+  if (minutes.toString().length < 2) {
+    minutes = `0${minutes}`;
+  }
+
+  let formattedSunrise = `${hours}:${minutes}`;
+
+  return formattedSunrise;
+}
+
+function formatSunset(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours.toString().length < 2) {
+    hours = `0${hours}`;
+  }
+
+  let minutes = date.getMinutes();
+  if (minutes.toString().length < 2) {
+    minutes = `0${minutes}`;
+  }
+
+  let formattedSunset = `${hours}:${minutes}`;
+
+  return formattedSunset;
+}
+
+function formatForecastDate(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let forecastDay = date.getDate();
+  let forecastMonth = date.getMonth() + 1;
+
+  if (forecastDay.toString().length < 2) {
+    forecastDay = `0${forecastDay}`;
+  }
+
+  if (forecastMonth.toString().length < 2) {
+    forecastMonth = `0${forecastMonth}`;
+  }
+
+  let formattedForecastDate = `${forecastDay}.${forecastMonth}`;
+  return formattedForecastDate;
+}
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
+function displayForecast(response) {
+  let accordionItems = ["One", "Two", "Three", "Four", "Five", "Six", "Seven"];
+  let forecast = response.data.daily;
+  let forecastElement = document.querySelector("#forecast");
+  let forecastHTML = `<div class="accordion" id="weekday-detailed">`;
+  forecast.forEach(function (forecastDay, index) {
+    if (index > 0 && index < 8) {
+      forecastHTML =
+        forecastHTML +
+        `<div class="accordion-item">
+       <h2 class="accordion-header dynamic-font" id="panelsStayOpen-heading${
+         accordionItems[index]
+       }">
+      <button
+        class="accordion-button collapsed dynamic-font" type="button" data-bs-toggle="collapse"
+        data-bs-target="#panelsStayOpen-collapse${accordionItems[index]}"
+        aria-expanded="false"
+        aria-controls="panelsStayOpen-collapse${accordionItems[index]}"
+      >
+        <div class="col-4">
+          <span class="weekday">${formatDay(forecastDay.dt)}
+          </span>, <span class="weekdate">${formatForecastDate(
+            forecastDay.dt
+          )}</span>
+        </div>
+        <div class="col-3 image">
+          <img id="weekly-weather-icon" class="image-middle"
+            src="https://openweathermap.org/img/wn/${
+              forecastDay.weather[0].icon
+            }@2x.png"/>
+        </div>
+        <div class="col-4 center index">
+          <strong id="weekday-temp-max">${Math.round(
+            forecastDay.temp.max
+          )}</strong> /
+          <span id="weekday-temp-min">${Math.round(
+            forecastDay.temp.min
+          )}</span><span>°C</span>
+        </div>
+      </button>
+    </h2>
+    <div
+      id="panelsStayOpen-collapse${accordionItems[index]}"
+      class="accordion-collapse collapse"
+      aria-labelledby="panelsStayOpen-heading${accordionItems[index]}">
+      <div class="accordion-body p-0 dynamic-font">
+        <ul>
+          <li>
+            <span class="iconify" data-icon="bi:sunrise" data-inline="false"
+            ></span>
+            <span class="text">morning</span>
+            <span class="index" id="temp-morning">
+            ${Math.round(forecastDay.temp.morn)}<span>°C</span>
+           </span>
+          </li>
+          <li>
+            <span class="iconify" data-icon="fontisto:day-sunny" data-inline="false"></span>
+            <span class="text">day</span>
+            <span class="index" id="temp-day">
+            ${Math.round(forecastDay.temp.day)}<span>°C</span>
+          </span>
+          </li>
+          <li>
+            <span class="iconify" data-icon="akar-icons:umbrella" data-inline="false"></span>
+            <span class="text">chance</span>
+            <span class="index" id="weekday-chance-of-rain">
+            ${Math.round(forecastDay.pop)}%
+           </span>
+          </li>
+          <li>
+            <span class="iconify" data-icon="wi:barometer" data-inline="false"></span>
+            <span class="text">pressure</span>
+            <span class="index" id="weekday-pressure">
+            ${forecastDay.pressure} hPa
+           </span>
+          </li>
+          <li>
+            <span class="iconify" data-icon="bi:wind" data-inline="false"></span>
+            <span class="text">w.speed</span>
+            <span class="index">
+            <span id="weekday-wind-speed">${Math.round(
+              forecastDay.wind_speed
+            )}</span>
+            <span>km/h</span>
+           </span>
+          </li>
+          <li>
+            <span class="iconify" data-icon="wi:sunrise" data-inline="false"></span>
+            <span class="text">sunrise</span>
+            <span class="index" id="weekday-sunrise">
+            ${formatSunrise(forecastDay.sunrise)}
+           </span>
+          </li>
+          <li>
+            <span class="iconify" data-icon="bi:sunset" data-inline="false"></span>
+            <span class="text">evening</span>
+            <span class="index" id="temp-evening">
+            ${Math.round(forecastDay.temp.eve)}<span>°C</span>
+          </span>
+          </li>
+          <li>
+            <span class="iconify" data-icon="mdi:weather-night" data-inline="false"></span>
+            <span class="text">night</span>
+            <span class="index" id="temp-night">
+            ${Math.round(forecastDay.temp.night)}<span>°C</span>
+           </span>
+          </li>
+          <li>
+            <span class="iconify" data-icon="akar-icons:cloud" data-inline="false"></span>
+            <span class="text">clouds</span>
+            <span class="index" id="weekday-cloudiness">
+             ${forecastDay.clouds}%
+            </span>
+          </li>
+          <li>
+            <span class="iconify" data-icon="mi:drop" data-inline="false"></span>
+            <span class="text">humidity</span>
+            <span class="index" id="weekday-humidity">
+             ${forecastDay.humidity}%
+           </span>
+          </li>
+          <li>
+            <span class="iconify" data-icon="carbon:uv-index-alt" data-inline="false"></span>
+            <span class="text">UV-index</span>
+            <span class="index" id="weekday-uv-index">
+             ${forecastDay.uvi}
+           </span>
+          </li>
+          <li>
+            <span class="iconify" data-icon="wi:sunset" data-inline="false"></span>
+            <span class="text">sunset</span>
+            <span class="index" id="weekday-sunset">
+            ${formatSunset(forecastDay.sunset)}
+           </span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>`;
+    }
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  let apiKey = `ad121762f7a5646c2417d19be72f81a3`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function showTemp(response) {
   document.querySelector("#current-temp").innerHTML = Math.round(
     response.data.main.temp
@@ -54,6 +264,7 @@ function showTemp(response) {
       `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
     );
   celsiusTemp = response.data.main.temp;
+  getForecast(response.data.coord);
 }
 
 function searchCity(city) {
@@ -79,6 +290,16 @@ function showPosition(position) {
 function getPosition(event) {
   event.preventDefault();
   navigator.geolocation.getCurrentPosition(showPosition);
+}
+
+var autocomplete;
+
+function initMap() {
+  autocomplete = new google.maps.places.Autocomplete(
+    document.getElementById("search-input"),
+    { types: ["geocode"] }
+  );
+  google.maps.event.addListener(autocomplete, "place_changed", function () {});
 }
 
 function showFahrenheit(event) {
